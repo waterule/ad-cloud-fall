16ed2cc1a116349626fade20d36e7ae09b9f65c8
waterule <waterule@163com>
2018-11-30 17:16:42 +0800

    fix sonar setting file name

--- service-user/src/main/java/com/newtouch/cloud/serviceuser/controller/NewAppUser.java
+++ service-user/src/main/java/com/newtouch/cloud/serviceuser/controller/NewAppUser.java
@@ -19,4 +19,27 @@ public class NewAppUser {
     @Pattern(regexp = "(\\w|\\p{Punct}){6,30}")
     private String password;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
